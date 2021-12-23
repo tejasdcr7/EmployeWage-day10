f64c9dc7ab520553112ba61fc792a1a6d34ee3e9
tejasdcr7 <tejasdandge@gmail.com>
2021-12-23 17:12:19 +0530

    added uc05

--- EmployeWage.java
+++ EmployeWage.java
@@ -4,24 +4,30 @@ public class EmployeWage {
     public static void main(String[] args) {
         final int fulltime=1;
         final int partime=2;
+        final int mothworkdays=20;
         int Rate=20;
         int Emphour=0;
         int Empwage=0;
+        int totalmonthwage=0;
 
+        for(int day=1;day<mothworkdays;day++){
+            int empcheck = (int) Math.floor(Math.random() * 10) % 3;
+            switch (empcheck) {
+                case fulltime:
+                    Emphour = 8;
+                    break;
 
-        int empcheck =(int) Math.floor(Math.random() * 10) % 3;
-        switch (empcheck) {
-            case fulltime:
-                Emphour = 8;
-                break;
-
-            case partime:
-                Emphour = 4;
-                break;
+                case partime:
+                    Emphour = 4;
+                    break;
+            }
+            Empwage = Emphour*Rate;
+            totalmonthwage=totalmonthwage+Empwage;
+            System.out.println("Empwage"+Empwage);
         }
-        Empwage = Emphour*Rate;
-        System.out.println("Empwage"+Empwage);
+        System.out.println("Totalmonthwage" +totalmonthwage);
     }
+
 
 }
 
