275b1a90c31b850a2508bc8e3c6b310603733fa1
tejasdcr7 <tejasdandge@gmail.com>
2021-12-23 17:05:33 +0530

    added uc02

--- EmployeWage.java
+++ EmployeWage.java
@@ -3,12 +3,21 @@ package com.bridgelabz.day10;
 public class EmployeWage {
     public static void main(String[] args) {
         int fulltime = 1;
+        int Rate=20;
+        int Emphour=0;
+        int Empwage=0;
+
         double empcheck = Math.floor(Math.random() * 10) % 2;
         if (empcheck == fulltime) {
             System.out.println("Emp is Present");
+            Emphour=8;
         }
-        else
+        else{
             System.out.println("Emp is absent");
+            Emphour=0;}
+
+        Empwage=Emphour*Rate;
+        System.out.println("Empwage"+Empwage);
     }
 
 }
