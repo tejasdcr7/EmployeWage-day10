7d7f065f49a8d416f78d7186d8d31f0cba316557
tejasdcr7 <tejasdandge@gmail.com>
2021-12-23 17:09:08 +0530

    added uc04

--- EmployeWage.java
+++ EmployeWage.java
@@ -2,23 +2,24 @@ package com.bridgelabz.day10;
 
 public class EmployeWage {
     public static void main(String[] args) {
-        int fulltime = 1;
-        int parttime = 2;
+        final int fulltime=1;
+        final int partime=2;
         int Rate=20;
         int Emphour=0;
         int Empwage=0;
 
 
-        double empcheck = Math.floor(Math.random() * 10) % 3;
-        if (empcheck == fulltime)
-            Emphour=8;
-        else if (empcheck == parttime)
-            Emphour=4;
-        else
-            Emphour=0;
+        int empcheck =(int) Math.floor(Math.random() * 10) % 3;
+        switch (empcheck) {
+            case fulltime:
+                Emphour = 8;
+                break;
 
-
-        Empwage=Emphour*Rate;
+            case partime:
+                Emphour = 4;
+                break;
+        }
+        Empwage = Emphour*Rate;
         System.out.println("Empwage"+Empwage);
     }
 
